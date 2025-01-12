8c41a2acbd48857c988ed5ec9563d36e2882b4fc
abdessamad <allaabdessamad21@gmail.com>
2025-01-12 21:16:57 +0100

    add api endpoint that send back language, ip and user agent infos

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const ip = req.socket.remoteAddress;
+  const lang = req.acceptsLanguages();
+  const userAgent = req.get('user-agent');
+  res.json({ipaddress: ip, language: lang.toString(), software: userAgent}).status(200);
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
